f185b4d927093df1c8083181bc820b852cab6b1b
Yulia Korolyova <yukorolyova1@yandex.ru>
2023-09-26 13:29:45 +0300

    14 проект1.35

--- middlewares/Forbidden.js
+++ middlewares/Forbidden.js
@@ -1,7 +1,7 @@
 class Forbidden extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = 404;
+    this.statusCode = 403;
   }
 }
 module.exports = Forbidden;
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,12 @@
-const { HTTP_STATUS_UNAUTHORIZED } = require('http2').constants;
 const jwt = require('jsonwebtoken');
+const Unauthorized = require('./Unauthorized');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(HTTP_STATUS_UNAUTHORIZED).send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'practicum2023');
   } catch (err) {
-    return res.status(HTTP_STATUS_UNAUTHORIZED).send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Необходима авторизация'));
   }
 
   req.user = payload;
